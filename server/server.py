ade3a4d7c0decec9c0a602e19cf71a8889f03ad6
Vinh <vinhpham123.np@gmail.com>
2023-10-17 00:59:53 +0700

    add read mail to server.py

--- server/server.py
+++ server/server.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 
 import os.path
+import base64
+import json
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -9,7 +11,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SCOPES = ['https://mail.google.com/']
 
 
 def main():
@@ -34,21 +36,54 @@ def main():
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
+    # try:
+    #     # Call the Gmail API
+    #     service = build('gmail', 'v1', credentials=creds)
+    #     results = service.users().labels().list(userId='me').execute()
+    #     labels = results.get('labels', [])
+
+    #     if not labels:
+    #         print('No labels found.')
+    #         return
+    #     print('Labels:')
+    #     for label in labels:
+    #         print(label['name'])
+
+    # except HttpError as error:
+    #     # TODO(developer) - Handle errors from gmail API.
+    #     print(f'An error occurred: {error}')
+
     try:
-        # Call the Gmail API
         service = build('gmail', 'v1', credentials=creds)
-        results = service.users().labels().list(userId='me').execute()
-        labels = results.get('labels', [])
+        results = service.users().messages().list(userId='me', labelIds=[
+            'INBOX', 'UNREAD'], includeSpamTrash='false', ).execute()
+        messages = results.get('messages', [])
+        if not messages:
+            print('No new messages.')
+        else:
+            for message in messages:
+                msg = service.users().messages().get(
+                    userId='me', id=message['id']).execute()
+                email_data = msg['payload']['headers']
+                for values in email_data:
+                    name = values['name']
+                    if name == 'From':
+                        from_name = values['value']
+                        for part in msg['payload']['parts']:
+                            try:
+                                data = part['body']["data"]
+                                byte_code = base64.urlsafe_b64decode(data)
 
-        if not labels:
-            print('No labels found.')
-            return
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
+                                text = byte_code.decode("utf-8")
+                                if (text[0] != '<'):
+                                    print("This is the message: " + str(text))
 
+                                # mark the message as read
+                                msg = service.users().messages().modify(userId='me', id=message['id'], body={
+                                    'removeLabelIds': ['UNREAD']}).execute()
+                            except BaseException as error:
+                                pass
     except HttpError as error:
-        # TODO(developer) - Handle errors from gmail API.
         print(f'An error occurred: {error}')
 
 
