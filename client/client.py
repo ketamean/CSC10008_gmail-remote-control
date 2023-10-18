16a5a0c06b594d4db78eec74c161451a9d2c72e4
Vinh <vinhpham123.np@gmail.com>
2023-10-18 23:16:44 +0700

    update client to send email

--- client/client.py
+++ client/client.py
@@ -1,7 +1,160 @@
-from flask import Flask
+from __future__ import print_function
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World! I am client</p>"
+import base64
+import mimetypes
+import os
+from email.message import EmailMessage
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
+
+import google.auth
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://mail.google.com/']
+
+
+def gmail_create_draft_with_attachment():
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    try:
+        # create gmail api client
+        service = build('gmail', 'v1', credentials=creds)
+        mime_message = EmailMessage()
+
+        # headers
+        mime_message['To'] = 'vinhpham123.np@gmail.com'
+        mime_message['From'] = 'vinhpt.ti.1922@gmail.com'
+        mime_message['Subject'] = 'sample with attachment'
+
+        # text
+        mime_message.set_content(
+            'Hi, this is automated mail with attachment.'
+            'Please do not reply.'
+        )
+
+        # attachment
+        attachment_filename = 'file.png'
+        # guessing the MIME type
+        type_subtype, _ = mimetypes.guess_type(attachment_filename)
+        maintype, subtype = type_subtype.split('/')
+
+        with open(attachment_filename, 'rb') as fp:
+            attachment_data = fp.read()
+        mime_message.add_attachment(attachment_data, maintype, subtype)
+
+        encoded_message = base64.urlsafe_b64encode(
+            mime_message.as_bytes()).decode()
+
+        create_draft_request_body = {
+            'message': {
+                'raw': encoded_message
+            }
+        }
+        # pylint: disable=E1101
+        draft = service.users().drafts().create(userId="me",
+                                                body=create_draft_request_body)\
+            .execute()
+        print(F'Draft id: {draft["id"]}\nDraft message: {draft["message"]}')
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        draft = None
+    return draft
+
+
+def gmail_send_message():
+
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    try:
+        service = build('gmail', 'v1', credentials=creds)
+        message = EmailMessage()
+
+        message.set_content('This is automated draft mail')
+
+        message['To'] = 'vinhpham123.np@gmail.com'
+        message['From'] = 'chiemthoica@gmail.com'
+        message['Subject'] = 'Automated draft'
+
+        # encoded message
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()) \
+            .decode()
+
+        attachment_filename = 'file.png'
+        # guessing the MIME type
+        type_subtype, _ = mimetypes.guess_type(attachment_filename)
+        maintype, subtype = type_subtype.split('/')
+
+        with open(attachment_filename, 'rb') as fp:
+            attachment_data = fp.read()
+        message.add_attachment(attachment_data, maintype, subtype)
+
+        encoded_message = base64.urlsafe_b64encode(
+            message.as_bytes()).decode()
+
+        create_message = {
+            'raw': encoded_message
+        }
+        # pylint: disable=E1101
+        send_message = (service.users().messages().send
+                        (userId="me", body=create_message).execute())
+        print(F'Message Id: {send_message["id"]}')
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        send_message = None
+    return send_message
+
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    # try:
+    #     # Call the Gmail API
+    #     service = build('gmail', 'v1', credentials=creds)
+    #     results = service.users().labels().list(userId='me').execute()
+    #     labels = results.get('labels', [])
+
+    #     if not labels:
+    #         print('No labels found.')
+    #         return
+    #     print('Labels:')
+    #     for label in labels:
+    #         print(label['name'])
+
+    # except HttpError as error:
+    #     # TODO(developer) - Handle errors from gmail API.
+    #     print(f'An error occurred: {error}')
+
+    gmail_send_message()
+
+
+if __name__ == '__main__':
+    main()
